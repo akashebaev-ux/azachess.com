b66f8052fb4845b24213226a97bc255ba0765dce
Azamat Kashebayev <AKashebaev@gmail.com>
2026-09-19 18:27:30 +0200

    Load OpenAI API key from environment

--- azachess/settings.py
+++ azachess/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(
+    BASE_DIR / ".env"
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
